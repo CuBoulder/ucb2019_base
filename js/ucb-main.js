8707ff847e454c3964d7bb6e59d0d209c5263fe7
jcsparks <jeremy@jcsparks.com>
2019-06-18 16:05:52 -0600

    Styling tweaks to look more like the UCB website.

--- js/ucb-main.js
+++ js/ucb-main.js
@@ -7,9 +7,8 @@
 jQuery(document).ready(function () {
 
     // handler to close the local tasks (edit, revisions, clone, etc...)
-    jQuery('#ucbCloseLocalTasks').click(function (e) {
+    jQuery('a#ucbCloseLocalTasks').click(function (e) {
         e.preventDefault();
-
         jQuery('.ucb-local-tasks').hide();
     });
 
