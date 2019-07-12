42909c5ed2031a936e9494786be572e35a540267
jcsparks <jeremy@jcsparks.com>
2019-07-12 16:46:36 -0600

    Adding in Vue code to work with pulling in data via JSON:API.

---
+++ js/ucb-organization.js
@@ -0,0 +1,58 @@
+
+Vue.component('ucb-promoted-children', {
+    props: {
+        dataurl: ''
+    },
+    data: function() {
+        return {
+            error: '',
+            posts: []
+        }
+    },
+    mounted() { // when the Vue app is booted up, this is run automatically.
+        var self = this // create a closure to access component in the callback below
+        let jsonURL = self.dataurl;
+        if(jsonURL !== '') {
+            jQuery.getJSON(jsonURL, function (data) {
+                if(data.data) {
+                    self.posts = data.data;
+                }
+            }).fail(function () {
+                self.error = "Unable to pare JSON data from specified URL.";
+            });
+        }else {
+            self.error = 'Data URL not defined, please check your configuration and retry your request.';
+        }
+    }
+});
+
+Vue.component('ucb-standard-children', {
+    props: {
+        dataurl: ''
+    },
+    data: function() {
+        return {
+            error: '',
+            posts: []
+        }
+    },
+    mounted() { // when the Vue app is booted up, this is run automatically.
+        var self = this // create a closure to access component in the callback below
+        let jsonURL = self.dataurl;
+        if(jsonURL !== '') {
+            jQuery.getJSON(jsonURL, function (data) {
+                if(data.data) {
+                    self.posts = data.data;
+                }
+            }).fail(function () {
+                self.error = "Unable to pare JSON data from specified URL.";
+            });
+        }else {
+            self.error = 'Data URL not defined, please check your configuration and retry your request.';
+        }
+    }
+});
+
+var app = new Vue({
+    el: '#vue-organization-children',
+});
