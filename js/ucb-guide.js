f14c9cd4add9e9371568afb1236bb50332473885
Jeremy Sparks <jeremy@jcsparks.com>
2019-09-25 09:30:18 -0600

    Updated JS code for the Guide page dealing with the jump menu to remedy issues #4 and #5.

--- js/ucb-guide.js
+++ js/ucb-guide.js
@@ -18,5 +18,10 @@ function buildJumpNav() {
           jQuery('<a>').attr('href', '#' + sectId).append(sectLinkName)));
     }
   })
+
+  // check to make sure we've got some jump links in the jump nav
+  if(jQuery('#ucb-jump-nav li').length > 1) {
+    jQuery('#ucb-jump-nav').css('display', 'flex');
+  }
 }
 
