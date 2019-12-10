09b7b5bb12038ed1b667bfbe013aef090555f978
Jeremy Sparks <jeremy@jcsparks.com>
2019-12-10 09:01:11 -0700

    chg: Adding in theme layout/styling for the view-based homepage for Alerts based on Active Incidents.

---
+++ js/ucb-incident.js
@@ -0,0 +1,44 @@
+jQuery(document).ready(function () {
+  // Helper function for the Incident View
+  IncidentView();
+
+  //Handler to toggle the visibility of multiple Incident Nodes
+  jQuery('a.ucb-incident-node-toggle').click(function (e) {
+    e.preventDefault();
+    var myTarget = jQuery(this).data('target');
+
+    // hide all of the node items
+    jQuery('ul.incident-list>li').hide();
+    jQuery('a.ucb-incident-node-toggle.active').removeClass('active');
+
+    // show the node item that they want
+    jQuery('h1#'+myTarget).closest('ul.incident-list li').show();
+
+    // set the active flag on the currently selected button
+    jQuery('a.ucb-incident-node-toggle[data-target=' + myTarget + ']').addClass('active');
+  })
+});
+
+function IncidentView() {
+  // Determine how many active Incidents we have to work with.
+  var IncidentList = jQuery('ul.incident-list');
+
+  // If we have more than one active Incident then there's some work to do
+  if( IncidentList && IncidentList.children().length > 1 ) {
+    // add in a control structure to allow the user to toggle between different Incidents
+    addIncidentToggle();
+  }
+}
+
+function addIncidentToggle() {
+  // grab the ids from the titles of each node and use those to create a button
+  // to toggle between those nodes
+  var IncidentNodes = jQuery('.ucb-incident-title-col h1');
+
+  jQuery.each(IncidentNodes, function (index, value) {
+    var myID = value.getAttribute('id');
+    var myTitle = jQuery(value).find('span').html();
+    jQuery('ol.ucb-incident-control-list').append("<li class='ucb-incident-toggle-li'><a href='#' class='ucb-incident-node-toggle' data-target='" + myID + "'>" + myTitle +"</a></li>");
+  })
+
+}
